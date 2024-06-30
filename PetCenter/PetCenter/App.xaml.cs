d4371e44fe88ba89d8be85cbbb794cb068e1bdf4
FilipT03 <filipsombor@gmail.com>
2024-06-30 12:55:04 +0200

    Add HostBuilder to App class

--- PetCenter/PetCenter/App.xaml.cs
+++ PetCenter/PetCenter/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,27 @@ namespace PetCenter
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+            => _host = CreateHostBuilder().Build();
+
+        private static IHostBuilder CreateHostBuilder(string[]? args = null)
+            => Host.CreateDefaultBuilder(args);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+            Window window = _host.Services.GetRequiredService<MainWindow>();
+            window.Show();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync();
+            base.OnExit(e);
+        }
     }
 
 }
