bb74c381cb995730103c82d0cbd0ce151366ba1d
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-06-30 16:17:11 +0200

    feat: Add entity framework

---
+++ PetCenter/PetCenter/Repository/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetCenter.Domain.Model;
+
+namespace PetCenter.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("");
+            }
+        }
+    }
+}
