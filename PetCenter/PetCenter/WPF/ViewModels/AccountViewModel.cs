cb27f232293c06c93c289fd15a966fad009cf605
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-01 17:04:37 +0200

    feat: Add account viewmodel and validation rules

---
+++ PetCenter/PetCenter/WPF/ViewModels/AccountViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetCenter.Domain.Model;
+
+namespace PetCenter.WPF.ViewModels
+{
+    public class AccountViewModel(Account account) : ViewModelBase
+    {
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Person? Person
+        {
+            get => _person;
+            set
+            {
+                _person = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _username = account.Username;
+        private string _email = account.Email;
+        private string _password = account.Password;
+        private Person? _person = account.Person;
+    }
+}
