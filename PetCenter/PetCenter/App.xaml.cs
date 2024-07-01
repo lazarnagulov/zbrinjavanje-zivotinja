218334d929453b621791baf30aaf8fc186689267
FilipT03 <filipsombor@gmail.com>
2024-07-02 01:14:26 +0200

    feat: Add Host and host building

--- PetCenter/PetCenter/App.xaml.cs
+++ PetCenter/PetCenter/App.xaml.cs
@@ -3,6 +3,11 @@ using System.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Data;
 using System.Windows;
+using PetCenter.WPF.Views;
+using PetCenter.HostBuilders;
+using LangLang.HostBuilders;
+using PetCenter.Core.Stores;
+using PetCenter.WPF.ViewModels;
 
 namespace PetCenter
 {
@@ -17,13 +22,20 @@ namespace PetCenter
             => _host = CreateHostBuilder().Build();
 
         private static IHostBuilder CreateHostBuilder(string[]? args = null)
-            => Host.CreateDefaultBuilder(args);
+            => Host.CreateDefaultBuilder(args)
+                   .AddViewModels()
+                   .AddStores()
+                   .AddWindows();
 
         protected override void OnStartup(StartupEventArgs e)
         {
             _host.Start();
+
             Window window = _host.Services.GetRequiredService<MainWindow>();
+            NavigationStore navigationStore = _host.Services.GetRequiredService<NavigationStore>();
+            navigationStore.CurrentViewModel = _host.Services.GetRequiredService<Test1ViewModel>();
             window.Show();
+
             base.OnStartup(e);
         }
 
---
+++ PetCenter/PetCenter/HostBuilders/AddStoresExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PetCenter.Core.Stores;
+using PetCenter.WPF.ViewModels;
+
+namespace LangLang.HostBuilders;
+
+public static class AddStoresExtension
+{
+    public static IHostBuilder AddStores(this IHostBuilder host)
+    {
+        host.ConfigureServices(services =>
+        {
+            services.AddSingleton<NavigationStore>();
+        });
+        
+        return host;
+    }
+}
