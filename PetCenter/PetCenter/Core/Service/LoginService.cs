e083cbcffc53c1eed8ed2cc0648bcfae82f888c7
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-02 18:37:49 +0200

    feat: Add authentication store

--- PetCenter/PetCenter/Core/Service/LoginService.cs
+++ PetCenter/PetCenter/Core/Service/LoginService.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
+using PetCenter.Core.Stores;
 using PetCenter.Core.Util;
 using PetCenter.Domain.Model;
 using PetCenter.Domain.RepositoryInterfaces;
 
 namespace PetCenter.Core.Service
 {
-    public class LoginService(IAccountRepository accountRepository)
+    public class LoginService(IAccountRepository accountRepository, AuthenticationStore authenticationStore)
     {
         public Account? Login(string username, string password)
-            => accountRepository.Authenticate(username, PasswordEncoder.Encode(password));
-
+        {
+            var account = accountRepository.Authenticate(username, password);
+            authenticationStore.CurrentUserProfile = account;
+            return account;
+        }
     }
 }
