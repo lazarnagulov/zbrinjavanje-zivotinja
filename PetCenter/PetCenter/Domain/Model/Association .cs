2252f56908fe086aa977d4091b7e702d04ace1e3
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-03 18:54:02 +0200

    feat: Add association to db

--- PetCenter/PetCenter/Domain/Model/Association .cs
+++ PetCenter/PetCenter/Domain/Model/Association .cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PetCenter.Domain.Model
 {
+    [Table("association")]
     public class Association
     {
+        public Association()
+        {
+        }
+
+        public Association(string name, string accountNumber, string? websiteLink = null)
+        {
+            Name = name;
+            AccountNumber = accountNumber;
+            WebsiteLink = websiteLink;
+        }
+
+        [Key]
+        [Column("id_assoc")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MaxLength(30)]
+        [Required]
+        [Column("assoc_name")]
         public string Name { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        [Column("assoc_acc_number")]
         public string AccountNumber { get; set; }
-        public string WebSiteLink { get; set; }
+
+        [MaxLength(50)]
+        [Column("assoc_website")]
+        public string? WebsiteLink { get; set; }
     }
 }
---
+++ PetCenter/PetCenter/Migrations/20240703165331_Associations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetCenter.Migrations
+{
+    /// <inheritdoc />
+    public partial class Associations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id_mess",
+                table: "notification",
+                newName: "id_notification");
+
+            migrationBuilder.CreateTable(
+                name: "association",
+                columns: table => new
+                {
+                    id_assoc = table.Column<Guid>(type: "uuid", nullable: false),
+                    assoc_name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    assoc_acc_number = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    assoc_website = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_association", x => x.id_assoc);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "association");
+
+            migrationBuilder.RenameColumn(
+                name: "id_notification",
+                table: "notification",
+                newName: "id_mess");
+        }
+    }
+}
