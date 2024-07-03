cbf6eb543f1629d0848000fcadee5176eca1199a
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-03 18:47:13 +0200

    feat: Add association service

---
+++ PetCenter/PetCenter/Core/Service/AssociationService.cs
@@ -0,0 +1,18 @@
+﻿using PetCenter.Domain.Model;
+using PetCenter.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCenter.Core.Service
+{
+    public class AssociationService(IAssociationRepository associationRepository)
+    {
+        public bool Insert(Association post) => associationRepository.Insert(post);
+        public bool Delete(Association post) => associationRepository.Delete(post);
+        public Association? GetById(Guid id) => associationRepository.GetById(id);
+        public List<Association> GetAll() => associationRepository.GetAll();
+    }
+}
