0248c07444a465f06a19877e683a33ba203ca725
Lazar Nagulov <lazarnagulov1@gmail.com>
2024-07-05 16:46:38 +0200

    Update scripts

---
+++ scripts/dml.sql
@@ -0,0 +1,260 @@
+INSERT INTO public."__EFMigrationsHistory" VALUES ('20240701100756_Initial', '8.0.6');
+INSERT INTO public."__EFMigrationsHistory" VALUES ('20240702210817_Validation', '8.0.6');
+INSERT INTO public."__EFMigrationsHistory" VALUES ('20240703162751_Notifications', '8.0.6');
+INSERT INTO public."__EFMigrationsHistory" VALUES ('20240703165331_Associations', '8.0.6');
+
+INSERT INTO public.account VALUES ('e6246276-9ea7-4479-b6d8-87bdc825adcb', 'ilija', 'ilija@gmail.com', '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', 1, '4f374181-a9e9-41b7-b720-9cc482197c66');
+INSERT INTO public.account VALUES ('3cc22e32-c192-4cea-ab65-e6f2cb8eb0f4', 'vuk', 'vuk@gmail.com', '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', 0, '64e5676e-fdfa-4015-b62f-c3da5d3ee32d');
+INSERT INTO public.account VALUES ('d1138e33-6a17-4379-ab02-6c07cd006a83', 'testuser', 'email@email.com', 'zdnfQa3Mv16Fis1IKCr9fOFa8/4+5Yfq0Tg/tpFYUXw=', 1, 'cb131749-5857-43f9-be1b-e8a74aa66d1a');
+INSERT INTO public.account VALUES ('4f638144-85a6-11ec-b909-0242ac120002', 'johndoe', 'john@example.com', '9b1de1fce0c2e5971ee6f3bd0e1c88a0cf8e76e09f3f4a3f6d50d3bb1f1c7c5a', 1, '4f638094-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.account VALUES ('4f638170-85a6-11ec-b909-0242ac120002', 'janesmith', 'jane@example.com', 'e8c3b208f7383b4efb6ffb91d5a3b8241d96b54c03b7561f4e9b5f3e237e35c1', 2, '4f6380c0-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.account VALUES ('4f63819c-85a6-11ec-b909-0242ac120002', 'alicej', 'alice@example.com', 'b7e23ec29af22b0b4e41da31e868d57226121c84b07f7d4f67c5e2d76f6e2453', 1, '4f6380ec-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.account VALUES ('4f6381c8-85a6-11ec-b909-0242ac120002', 'bobbrown', 'bob@example.com', '3c3d1d3e9e833bc9c37ef4f13b4d4cb5e16e72d3e6e8c5a1f81e18a4566c4f79', 2, '4f638118-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.account VALUES ('f5b4aa72-51f1-4813-a378-b5a84bd0e9fd', 'lazar', 'lazar@gmail.com', '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', 2, '3207abde-c1cb-493c-95d4-c1914683207a');
+INSERT INTO public.account VALUES ('7d7d9143-0716-4a30-acc1-89bcfc0336ee', 'testic', 'test@gmail.com', 'pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=', 1, 'e2bf96ef-3ac9-486f-a7e1-f614b6adddd9');
+
+INSERT INTO public.address VALUES ('f367f088-fcbe-4758-9a93-453343b8fb6d', 'ulica', 1, 'Sombor', 'Srbija', 25000);
+INSERT INTO public.address VALUES ('f6921df4-380c-488f-804f-c68544bf394b', 'ulica', 1, 'Sombor', 'Srbija', 25000);
+INSERT INTO public.address VALUES ('5f6ef489-9fcf-4e57-bb10-f1555ca6b45c', 'ulica', 1, 'Sombor', 'Srbija', 25000);
+INSERT INTO public.address VALUES ('54b87dec-8de9-43fb-ae44-bc77b9840af1', 'neka', 1, 'neki', 'neka', 31233);
+INSERT INTO public.address VALUES ('5d7ba1da-0c07-4909-ab05-110eea65da57', 'neka', 1, 'neki', 'neka', 31233);
+INSERT INTO public.address VALUES ('cc3aab6e-9068-4e5c-b223-ccf7f1c77b5c', 'Perina ulica', 25, 'Perinov grad', 'Tunguzija', 42069);
+INSERT INTO public.address VALUES ('c2687397-bfc2-491d-a0aa-03abd4d70aac', 'Perina ulica', 12, 'Novi Sad', 'Srbija', 21000);
+INSERT INTO public.address VALUES ('4f637e64-85a6-11ec-b909-0242ac120002', 'Main St', 123, 'Springfield', 'USA', 12345);
+INSERT INTO public.address VALUES ('4f637e90-85a6-11ec-b909-0242ac120002', 'Elm St', 456, 'Shelbyville', 'USA', 67890);
+INSERT INTO public.address VALUES ('4f637ebc-85a6-11ec-b909-0242ac120002', 'Oak St', 789, 'Capital City', 'USA', 54321);
+INSERT INTO public.address VALUES ('4f637ee8-85a6-11ec-b909-0242ac120002', 'Pine St', 101, 'Ogdenville', 'USA', 98765);
+INSERT INTO public.address VALUES ('8bde475e-9d5c-4d37-802e-8b69731dd3a6', 'Filipa Tota', 10, 'Sombor', 'Srbija', 25000);
+INSERT INTO public.address VALUES ('6ff497fe-dcad-4f96-899a-d4907bb32612', 'Ilijina ulica', 15, 'Ilijingrad', 'Srbija', 151515);
+INSERT INTO public.address VALUES ('519dc5b4-fb6d-4a08-af49-12da665952f6', 'Vukova', 15, 'Novi Sad', 'Srbija', 21000);
+INSERT INTO public.address VALUES ('fc81c806-8127-4846-86af-6ee57131efd4', 'testiceva', 10, 'Testcity', 'Testonija', 22452);
+INSERT INTO public.address VALUES ('0eca5883-f718-4e34-946d-a93bf41c0e52', 'JNA', 10, 'Sombor', 'Srbija', 25000);
+
+INSERT INTO public.animal VALUES ('4f6380ec-85a6-11ec-b909-0242ac120002', '4f637f20-85a6-11ec-b909-0242ac120002', 'Buddy', 3, 'Friendly dog');
+INSERT INTO public.animal VALUES ('4f638118-85a6-11ec-b909-0242ac120002', '4f637f4c-85a6-11ec-b909-0242ac120002', 'Whiskers', 2, 'Curious cat');
+INSERT INTO public.animal VALUES ('4f6384f8-85a6-11ec-b909-0242ac120002', '4f637f78-85a6-11ec-b909-0242ac120002', 'Polly', 4, 'Colorful parrot');
+INSERT INTO public.animal VALUES ('4f638514-85a6-11ec-b909-0242ac120002', '4f637fa4-85a6-11ec-b909-0242ac120002', 'Max', 5, 'Energetic rabbit');
+INSERT INTO public.animal VALUES ('4f638530-85a6-11ec-b909-0242ac120002', '4f637f4c-85a6-11ec-b909-0242ac120002', 'Fluffy', 3, 'Playful hamster');
+
+INSERT INTO public.animal_type VALUES ('4f637f20-85a6-11ec-b909-0242ac120002', 'Dog', 'Labrador');
+INSERT INTO public.animal_type VALUES ('4f637f4c-85a6-11ec-b909-0242ac120002', 'Cat', 'Siamese');
+INSERT INTO public.animal_type VALUES ('4f637f78-85a6-11ec-b909-0242ac120002', 'Bird', 'Parrot');
+INSERT INTO public.animal_type VALUES ('4f637fa4-85a6-11ec-b909-0242ac120002', 'Rabbit', 'Angora');
+
+INSERT INTO public.association VALUES ('4f638118-85a6-11ec-b909-0242ac120069', 'Pet center', 'ACCT-1234567890', 'www.petcenter.rs');
+
+INSERT INTO public.comment VALUES ('55eefcf5-a7e4-4b58-8248-ed980c756984', '64e5676e-fdfa-4015-b62f-c3da5d3ee32d', 'She looks so good!');
+INSERT INTO public.comment VALUES ('aa0455d4-7850-4366-9fbb-2f2517b8dc00', '64e5676e-fdfa-4015-b62f-c3da5d3ee32d', 'Great looking parrot!');
+INSERT INTO public.comment VALUES ('b4486c84-a376-411b-b2ca-22b0b467bde3', '4f374181-a9e9-41b7-b720-9cc482197c66', 'Amazing photo!');
+INSERT INTO public.comment VALUES ('d1a48f09-2c7c-49d6-b934-33bf369a6869', '4f374181-a9e9-41b7-b720-9cc482197c66', 'I love that parrot!');
+
+INSERT INTO public.offer VALUES ('5a6848a1-5c4b-4765-8d8d-130bdd07f167', '64e5676e-fdfa-4015-b62f-c3da5d3ee32d', 1, 0);
+INSERT INTO public.offer VALUES ('edc5decb-7703-4baa-8994-d192804ca815', '4f374181-a9e9-41b7-b720-9cc482197c66', 0, 0);
+
+INSERT INTO public.person VALUES ('cb131749-5857-43f9-be1b-e8a74aa66d1a', 'Test', 'User', '11232414214', 1, '2014-07-01', '5d7ba1da-0c07-4909-ab05-110eea65da57');
+INSERT INTO public.person VALUES ('3207abde-c1cb-493c-95d4-c1914683207a', 'Lazar', 'Nagulov', '12213123123', 0, '2003-04-30', '0eca5883-f718-4e34-946d-a93bf41c0e52');
+INSERT INTO public.person VALUES ('4f638094-85a6-11ec-b909-0242ac120002', 'John', 'Doe', '555-1234', 1, '1980-01-01', '4f637e64-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.person VALUES ('4f6380c0-85a6-11ec-b909-0242ac120002', 'Jane', 'Smith', '555-5678', 2, '1990-02-02', '4f637e90-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.person VALUES ('4f6380ec-85a6-11ec-b909-0242ac120002', 'Alice', 'Johnson', '555-8765', 2, '1975-03-03', '4f637ebc-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.person VALUES ('4f638118-85a6-11ec-b909-0242ac120002', 'Bob', 'Brown', '555-4321', 1, '1985-04-04', '4f637ee8-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.person VALUES ('64e5676e-fdfa-4015-b62f-c3da5d3ee32d', 'Vuk', 'Vicentic', '31231231231', 0, '2024-08-03', '519dc5b4-fb6d-4a08-af49-12da665952f6');
+INSERT INTO public.person VALUES ('e2bf96ef-3ac9-486f-a7e1-f614b6adddd9', 'Test', 'Test', '132131241', 1, '2024-08-03', 'fc81c806-8127-4846-86af-6ee57131efd4');
+INSERT INTO public.person VALUES ('4f374181-a9e9-41b7-b720-9cc482197c66', 'Ilija', 'Jordanovski', '12346344142', 0, '2003-12-15', '6ff497fe-dcad-4f96-899a-d4907bb32612');
+INSERT INTO public.person VALUES ('246711ac-0359-433f-a279-8d1edefbeee1', 'tester', 'test', '1234567890', 0, '1980-06-06', '5f6ef489-9fcf-4e57-bb10-f1555ca6b45c');
+INSERT INTO public.person VALUES ('3467d9bc-abd6-4d06-bcca-23b88570fdc1', 'Pera', 'Peric', '1234567789', 0, '2000-03-03', 'cc3aab6e-9068-4e5c-b223-ccf7f1c77b5c');
+
+INSERT INTO public.photo VALUES ('4f638118-85a6-11ec-b909-0242ac120070', 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg', 'Cute cat', '4f638118-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.photo VALUES ('4fe380ec-85a6-11ec-b909-0242ac120002', 'https://images.pexels.com/photos/1001976/pexels-photo-1001976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'Fiendly dog', '4f6380ec-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.photo VALUES ('4fe380ec-85a6-11ec-b909-0242ac120069', 'https://images.pexels.com/photos/3397939/pexels-photo-3397939.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'Young picture', '4f6380ec-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.photo VALUES ('4ff38118-85a6-11ec-b909-0242ac120070', 'https://images.pexels.com/photos/1463295/pexels-photo-1463295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 'Great looking parrot', '4f6384f8-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.photo VALUES ('4ff38118-85a6-11ec-c909-0242ac120070', 'https://cdn.britannica.com/20/194520-050-DCAE62F1/New-World-Sylvilagus-cottontail-rabbits.jpg', 'My favourite rabbit', '4f638514-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.photo VALUES ('4ff38118-85a6-11ec-c909-0242ac126970', 'https://cdn.britannica.com/20/194520-050-DCAE62F1/New-World-Sylvilagus-cottontail-rabbits.jpgCute little hamster', 'Cute little hamster', '4f638530-85a6-11ec-b909-0242ac120002');
+
+INSERT INTO public.post VALUES ('4f6383e0-85a6-11ec-b909-0242ac120002', '4f638094-85a6-11ec-b909-0242ac120002', 'Check out my new puppy!', '4f6380ec-85a6-11ec-b909-0242ac120002', '2022-01-01');
+INSERT INTO public.post VALUES ('4f63840c-85a6-11ec-b909-0242ac120002', '4f6380c0-85a6-11ec-b909-0242ac120002', 'Look at my kitten!', '4f638118-85a6-11ec-b909-0242ac120002', '2022-02-02');
+INSERT INTO public.post VALUES ('4f638438-85a6-11ec-b909-0242ac120002', '4f6380ec-85a6-11ec-b909-0242ac120002', 'My parrot is so colorful!', '4f6384f8-85a6-11ec-b909-0242ac120002', '2022-03-03');
+INSERT INTO public.post VALUES ('4f638464-85a6-11ec-b909-0242ac120002', '4f638118-85a6-11ec-b909-0242ac120002', 'My rabbit is full of energy!', '4f638514-85a6-11ec-b909-0242ac120002', '2022-04-04');
+
+INSERT INTO public.post_comments VALUES ('55eefcf5-a7e4-4b58-8248-ed980c756984', '4f63840c-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_comments VALUES ('aa0455d4-7850-4366-9fbb-2f2517b8dc00', '4f638438-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_comments VALUES ('b4486c84-a376-411b-b2ca-22b0b467bde3', '4f63840c-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_comments VALUES ('d1a48f09-2c7c-49d6-b934-33bf369a6869', '4f638438-85a6-11ec-b909-0242ac120002');
+
+INSERT INTO public.post_likes VALUES ('3207abde-c1cb-493c-95d4-c1914683207a', '4f638438-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_likes VALUES ('64e5676e-fdfa-4015-b62f-c3da5d3ee32d', '4f63840c-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_likes VALUES ('4f374181-a9e9-41b7-b720-9cc482197c66', '4f63840c-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_likes VALUES ('4f638118-85a6-11ec-b909-0242ac120002', '4f638438-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_likes VALUES ('64e5676e-fdfa-4015-b62f-c3da5d3ee32d', '4f638438-85a6-11ec-b909-0242ac120002');
+
+INSERT INTO public.post_offers VALUES ('5a6848a1-5c4b-4765-8d8d-130bdd07f167', '4f63840c-85a6-11ec-b909-0242ac120002');
+INSERT INTO public.post_offers VALUES ('edc5decb-7703-4baa-8994-d192804ca815', '4f63840c-85a6-11ec-b909-0242ac120002');
+
+INSERT INTO public.post_state VALUES ('4f638438-85a6-11ec-b909-0242ac120002', 'TemporaryAccommodation');
+INSERT INTO public.post_state VALUES ('4f6383e0-85a6-11ec-b909-0242ac120002', 'OnHold');
+INSERT INTO public.post_state VALUES ('4f63840c-85a6-11ec-b909-0242ac120002', 'Accepted');
+INSERT INTO public.post_state VALUES ('4f638464-85a6-11ec-b909-0242ac120002', 'OnHold');
+
+INSERT INTO public.request VALUES ('6969abde-c1cb-493c-95d4-c1914683207a', 'cb131749-5857-43f9-be1b-e8a74aa66d1a', '2024-07-02', 2, 0);
+INSERT INTO public.request VALUES ('c9d4c349-36ca-4b6c-9a11-e3e57b163a35', '64e5676e-fdfa-4015-b62f-c3da5d3ee32d', '2024-07-05', 1, 0);
+
+INSERT INTO public.vote VALUES ('4f374181-a9e9-41b7-b720-9cc482197c66', 'c9d4c349-36ca-4b6c-9a11-e3e57b163a35', false);
+
+ALTER TABLE ONLY public."__EFMigrationsHistory"
+    ADD CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId");
+
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT "PK_account" PRIMARY KEY (id_acc);
+
+ALTER TABLE ONLY public.address
+    ADD CONSTRAINT "PK_address" PRIMARY KEY (id_adr);
+
+ALTER TABLE ONLY public.animal
+    ADD CONSTRAINT "PK_animal" PRIMARY KEY (id_animal);
+
+ALTER TABLE ONLY public.animal_type
+    ADD CONSTRAINT "PK_animal_type" PRIMARY KEY (id_animal_type);
+
+ALTER TABLE ONLY public.association
+    ADD CONSTRAINT "PK_association" PRIMARY KEY (id_assoc);
+
+ALTER TABLE ONLY public.comment
+    ADD CONSTRAINT "PK_comment" PRIMARY KEY (id_comment);
+
+ALTER TABLE ONLY public.notification
+    ADD CONSTRAINT "PK_notification" PRIMARY KEY (id_notification);
+
+ALTER TABLE ONLY public.offer
+    ADD CONSTRAINT "PK_offer" PRIMARY KEY (id_offer);
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT "PK_person" PRIMARY KEY (id_person);
+
+ALTER TABLE ONLY public.photo
+    ADD CONSTRAINT "PK_photo" PRIMARY KEY (id_photo);
+
+ALTER TABLE ONLY public.post
+    ADD CONSTRAINT "PK_post" PRIMARY KEY (id_post);
+
+ALTER TABLE ONLY public.post_comments
+    ADD CONSTRAINT "PK_post_comments" PRIMARY KEY (comment_id_comment, post_id_post_cmt);
+
+ALTER TABLE ONLY public.post_likes
+    ADD CONSTRAINT "PK_post_likes" PRIMARY KEY (person_id_liked, post_id_liked);
+
+ALTER TABLE ONLY public.post_offers
+    ADD CONSTRAINT "PK_post_offers" PRIMARY KEY (offer_id_offer, post_id_post);
+
+ALTER TABLE ONLY public.post_state
+    ADD CONSTRAINT "PK_post_state" PRIMARY KEY ("Id");
+
+ALTER TABLE ONLY public.request
+    ADD CONSTRAINT "PK_request" PRIMARY KEY (id_req);
+
+ALTER TABLE ONLY public.review
+    ADD CONSTRAINT "PK_review" PRIMARY KEY (id_review);
+
+ALTER TABLE ONLY public.vote
+    ADD CONSTRAINT "PK_vote" PRIMARY KEY (person_id_voter, request_id_voted);
+
+CREATE UNIQUE INDEX "IX_account_email" ON public.account USING btree (email);
+
+CREATE UNIQUE INDEX "IX_account_person_id_person" ON public.account USING btree (person_id_person);
+
+CREATE UNIQUE INDEX "IX_account_username" ON public.account USING btree (username);
+
+CREATE INDEX "IX_animal_animal_type_id_type" ON public.animal USING btree (animal_type_id_type);
+
+CREATE INDEX "IX_comment_person_author_c" ON public.comment USING btree (person_author_c);
+
+CREATE INDEX "IX_notification_id_recipient" ON public.notification USING btree (id_recipient);
+
+CREATE INDEX "IX_offer_person_author_o" ON public.offer USING btree (person_author_o);
+
+CREATE INDEX "IX_person_address_id_adr" ON public.person USING btree (address_id_adr);
+
+CREATE INDEX "IX_photo_AnimalId" ON public.photo USING btree ("AnimalId");
+
+CREATE INDEX "IX_post_animal_animal_id" ON public.post USING btree (animal_animal_id);
+
+CREATE INDEX "IX_post_comments_post_id_post_cmt" ON public.post_comments USING btree (post_id_post_cmt);
+
+CREATE INDEX "IX_post_likes_post_id_liked" ON public.post_likes USING btree (post_id_liked);
+
+CREATE INDEX "IX_post_offers_post_id_post" ON public.post_offers USING btree (post_id_post);
+
+CREATE INDEX "IX_post_person_author_p" ON public.post USING btree (person_author_p);
+
+CREATE INDEX "IX_request_person_req_author" ON public.request USING btree (person_req_author);
+
+CREATE INDEX "IX_review_OfferId" ON public.review USING btree ("OfferId");
+
+CREATE INDEX "IX_vote_request_id_voted" ON public.vote USING btree (request_id_voted);
+
+CREATE TRIGGER trg_check_valid_requester BEFORE INSERT OR UPDATE ON public.request FOR EACH ROW EXECUTE FUNCTION public.fn_validate_request();
+
+CREATE TRIGGER trg_validate_request BEFORE INSERT OR UPDATE ON public.request FOR EACH ROW EXECUTE FUNCTION public.fn_validate_request();
+
+CREATE TRIGGER trigger_check_request BEFORE INSERT OR UPDATE ON public.request FOR EACH ROW EXECUTE FUNCTION public.fn_validate_request();
+
+CREATE TRIGGER trigger_check_statetype BEFORE INSERT OR UPDATE ON public.post_state FOR EACH ROW EXECUTE FUNCTION public.fn_check_statetype();
+
+ALTER TABLE ONLY public.account
+    ADD CONSTRAINT "FK_account_person_person_id_person" FOREIGN KEY (person_id_person) REFERENCES public.person(id_person);
+
+ALTER TABLE ONLY public.animal
+    ADD CONSTRAINT "FK_animal_animal_type_animal_type_id_type" FOREIGN KEY (animal_type_id_type) REFERENCES public.animal_type(id_animal_type) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.comment
+    ADD CONSTRAINT "FK_comment_person_person_author_c" FOREIGN KEY (person_author_c) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.notification
+    ADD CONSTRAINT "FK_notification_person_id_recipient" FOREIGN KEY (id_recipient) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.offer
+    ADD CONSTRAINT "FK_offer_person_person_author_o" FOREIGN KEY (person_author_o) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.person
+    ADD CONSTRAINT "FK_person_address_address_id_adr" FOREIGN KEY (address_id_adr) REFERENCES public.address(id_adr) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.photo
+    ADD CONSTRAINT "FK_photo_animal_AnimalId" FOREIGN KEY ("AnimalId") REFERENCES public.animal(id_animal);
+
+ALTER TABLE ONLY public.post
+    ADD CONSTRAINT "FK_post_animal_animal_animal_id" FOREIGN KEY (animal_animal_id) REFERENCES public.animal(id_animal) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_comments
+    ADD CONSTRAINT "FK_post_comments_comment_comment_id_comment" FOREIGN KEY (comment_id_comment) REFERENCES public.comment(id_comment) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_comments
+    ADD CONSTRAINT "FK_post_comments_post_post_id_post_cmt" FOREIGN KEY (post_id_post_cmt) REFERENCES public.post(id_post) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_likes
+    ADD CONSTRAINT "FK_post_likes_person_person_id_liked" FOREIGN KEY (person_id_liked) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_likes
+    ADD CONSTRAINT "FK_post_likes_post_post_id_liked" FOREIGN KEY (post_id_liked) REFERENCES public.post(id_post) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_offers
+    ADD CONSTRAINT "FK_post_offers_offer_offer_id_offer" FOREIGN KEY (offer_id_offer) REFERENCES public.offer(id_offer) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_offers
+    ADD CONSTRAINT "FK_post_offers_post_post_id_post" FOREIGN KEY (post_id_post) REFERENCES public.post(id_post) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post
+    ADD CONSTRAINT "FK_post_person_person_author_p" FOREIGN KEY (person_author_p) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.post_state
+    ADD CONSTRAINT "FK_post_state_post_Id" FOREIGN KEY ("Id") REFERENCES public.post(id_post) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.request
+    ADD CONSTRAINT "FK_request_person_person_req_author" FOREIGN KEY (person_req_author) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.review
+    ADD CONSTRAINT "FK_review_offer_OfferId" FOREIGN KEY ("OfferId") REFERENCES public.offer(id_offer);
+
+ALTER TABLE ONLY public.vote
+    ADD CONSTRAINT "FK_vote_person_person_id_voter" FOREIGN KEY (person_id_voter) REFERENCES public.person(id_person) ON DELETE CASCADE;
+
+ALTER TABLE ONLY public.vote
+    ADD CONSTRAINT "FK_vote_request_request_id_voted" FOREIGN KEY (request_id_voted) REFERENCES public.request(id_req) ON DELETE CASCADE;
+
